a04b28a9ea9f0ff3cdc42ee436b8911d4a187baf
vickybarbieri0 <mabarbieri@frba.utn.edu.ar>
2024-04-21 19:21:51 -0300

    Implementar PersonaVulnerable

--- src/main/java/ar/edu/utn/frba/dds/PersonaVulnerable.java
+++ src/main/java/ar/edu/utn/frba/dds/PersonaVulnerable.java
@@ -1,4 +1,32 @@
 package ar.edu.utn.frba.dds;
+import java.util.Date;
 
 public class PersonaVulnerable {
+	private String nombre;
+	private String domicilio;
+	private Date fechaNacimiento;
+	private Integer menoresACargo;
+
+	public PersonaVulnerable(String nombre, String domicilio, Date fechaNacimiento, Integer menoresACargo) {
+		this.nombre = nombre;
+		this.domicilio = domicilio;
+		this.fechaNacimiento = fechaNacimiento;
+		this.menoresACargo = menoresACargo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public Integer getMenoresACargo() {
+		return menoresACargo;
+	}
 }
---
+++ src/main/java/ar/edu/utn/frba/dds/Usuario.java
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.dds;
+
+public abstract class Usuario {
+	private String nombre;
+	private String contrasenia;
+
+
+}
