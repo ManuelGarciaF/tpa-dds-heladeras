4749bc37930fe625e3db0a8d5b10a335eb4942b1
matias-rainhart <83478122+matias-rainhart@users.noreply.github.com>
2024-06-04 17:18:55 -0300

    corregir validacionContraseniasComunes

--- src/main/java/ar/edu/utn/frba/dds/dominio/Usuario.java
+++ src/main/java/ar/edu/utn/frba/dds/dominio/Usuario.java
@@ -2,14 +2,14 @@ package ar.edu.utn.frba.dds.dominio;
 
 import ar.edu.utn.frba.dds.dominio.validaciones.ValidacionContrasenia;
 import ar.edu.utn.frba.dds.dominio.validaciones.ValidacionLongitud;
-import ar.edu.utn.frba.dds.dominio.validaciones.ValidacionTop10k;
+import ar.edu.utn.frba.dds.dominio.validaciones.ValidacionContraseniasComunes;
 import ar.edu.utn.frba.dds.exceptions.ContraseniaInseguraException;
 import java.util.List;
 
 public class Usuario {
   private static final List<ValidacionContrasenia> validaciones = List.of(
       new ValidacionLongitud(8),
-      new ValidacionTop10k());
+      new ValidacionContraseniasComunes("10k-most-common.txt"));
   private final String nombre;
   private final String contrasenia;
 
---
+++ src/main/java/ar/edu/utn/frba/dds/dominio/validaciones/ValidacionContraseniasComunes.java
@@ -0,0 +1,26 @@
+package ar.edu.utn.frba.dds.dominio.validaciones;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ValidacionContraseniasComunes implements ValidacionContrasenia {
+  private BufferedReader readerArchivo;
+
+  public ValidacionContraseniasComunes(String nombreRecurso){
+    InputStream inputStream = getClass()
+        .getClassLoader()
+        .getResourceAsStream(nombreRecurso);
+    if (inputStream == null) {
+      throw new RuntimeException("No se pudo abrir el archivo de contrasenias");
+    }
+    readerArchivo = new BufferedReader(new InputStreamReader(inputStream));
+  }
+  @Override
+  public Boolean validar(String contrasenia) {
+    return readerArchivo.lines()
+        .noneMatch(line -> line.equals(contrasenia));
+  }
+
+
+}
