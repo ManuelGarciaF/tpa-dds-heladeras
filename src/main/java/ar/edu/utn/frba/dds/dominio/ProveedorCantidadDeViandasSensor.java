6e8f1ada6b1744c16306a3441b618ee6a4eef176
GerexOZ <gerortiz@frba.utn.edu.ar>
2024-08-16 20:22:39 -0300

    Se añadio un borrador del adapter del sensor de cantidad de viandas

---
+++ src/main/java/ar/edu/utn/frba/dds/dominio/ProveedorCantidadDeViandasSensor.java
@@ -0,0 +1,22 @@
+package ar.edu.utn.frba.dds.dominio;
+
+import ar.edu.utn.frba.dds.externo.LSensor;
+import java.time.LocalDateTime;
+
+public class ProveedorCantidadDeViandasSensor implements ProveedorCantidadDeViandas{
+  private Integer cantidadDeViandasParaNotificar;
+  private LocalDateTime ultimaLectura;
+
+  public ProveedorCantidadDeViandasSensor(LSensor sensor, Integer cantidadDeViandasParaNotificar) {
+    this.cantidadDeViandasParaNotificar = cantidadDeViandasParaNotificar;
+    sensor.onLunchBoxChanged(this::interpretarLectura);
+  }
+
+  @Override
+  public void interpretarLectura(int nuevaCantidad) {
+    ultimaLectura = LocalDateTime.now();
+    if (nuevaCantidad <= cantidadDeViandasParaNotificar) {
+      //NOTIFICAR A LAS PERSONAS
+    }
+  }
+}
