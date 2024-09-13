f58401c29df78c64c9ba1a140969ccb7e88697c1
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-09-13 11:21:19 -0300

    agregar persistence.xml para tests y arreglar main

--- src/main/java/ar/edu/utn/frba/dds/Main.java
+++ src/main/java/ar/edu/utn/frba/dds/Main.java
@@ -3,35 +3,38 @@ package ar.edu.utn.frba.dds;
 import ar.edu.utn.frba.dds.dominio.Heladera;
 import ar.edu.utn.frba.dds.dominio.MapaHeladeras;
 import ar.edu.utn.frba.dds.dominio.Ubicacion;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import java.time.LocalDate;
 
-public class Main {
+public class Main implements WithSimplePersistenceUnit {
   public static void main(String[] args) {
+    // Main main = new Main();
+    // main.agregarHeladera();
+
     // Cron:
     // */15 * * * * java -jar tpa.jar
 
-    // Seria reemplazado por acceso a la DB
-    Heladera unaHeladera = new Heladera(
-        "UTN Campus",
-        6,
-        new Ubicacion(5.35, 2.76),
-        "AA26U",
-        LocalDate.now(),
-        15.0,
-        0.0,
-        null,
-        null,
-        null,
-        null,
-        null
-    );
-    // MapaHeladeras.getInstance().agregarHeladera(unaHeladera);
-
-    revisarSensores();
-  }
-
-  private static void revisarSensores() {
     // Checkear fallos de conexion
     MapaHeladeras.getInstance().revisarSensoresDeTemperatura();
+  }
+
+  public void agregarHeladera() {
+    withTransaction(() -> {
+      Heladera unaHeladera = new Heladera(
+          "UTN Campus",
+          6,
+          new Ubicacion(5.35, 2.76),
+          "AA26U",
+          LocalDate.now(),
+          15.0,
+          0.0,
+          null,
+          null,
+          null,
+          null,
+          null
+      );
+      MapaHeladeras.getInstance().agregarHeladera(unaHeladera);
+    });
   }
 }
